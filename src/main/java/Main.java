0eadff621c262c1ce03cb1e7155e6d7905380f40
James Hartanto <jameshartanto1993@gmail.com>
2017-03-06 10:42:49 -0500

    James

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import javafx.scene.effect.Light;
+
 /**
  * In this exercise you will add a method to the provided LightSwitch class and
  * then use that class in the main method, as instructed below.
@@ -9,12 +11,15 @@ public class Main {
     public static void main(String[] args) {
 
         // todo: create a new instance of the LightSwitch and store it in a variable named lightSwitch.
+        LightSwitch lightSwitch = new LightSwitch();
 
         // todo: print the state of the light switch. This should read "The light switch is on: true" or "The light switch is on: false" depending on the state of the lightSwitch.
+        System.out.println("The light switch is on: " + lightSwitch.on);
 
         // todo: toggle the state of the light switch
+        lightSwitch.toggle();
 
         // todo: print the state of the light switch again. This should read "The light switch is on: true" or "The light switch is on: false" depending on the state of the lightSwitch.
-
+        System.out.println("The light switch is on: " + lightSwitch.on);
     }
 }
